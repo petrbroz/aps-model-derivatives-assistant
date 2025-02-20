3ddc5ab59f8cc5dd42443a707294bc6571fd90a5
Petr Broz <petr.broz@autodesk.com>
2025-02-20 15:47:36 +0100

    Small update in loading of external markdown content, and in logging.

--- agents/sqlite_agent.py
+++ agents/sqlite_agent.py
@@ -9,11 +9,16 @@ from langgraph.prebuilt import create_react_agent
 
 _llm = ChatOpenAI(model="gpt-4o")
 
+def _load_content(relative_path: str) -> str:
+    with open(os.path.join(os.path.dirname(__file__), relative_path)) as f:
+        return f.read()
+
 class SqliteAgent:
     def __init__(self, db: SQLDatabase, cache_urn_dir: str):
         sql_toolkit = SQLDatabaseToolkit(db=db, llm=_llm)
-        with open(os.path.join(os.path.dirname(__file__), "SYSTEM_PROMPTS.md")) as f:
-            system_prompts = f.read()
+        system_prompts = [
+            _load_content("SYSTEM_PROMPTS.md").replace("{", "{{").replace("}", "}}"),
+        ]
         prompt_template = ChatPromptTemplate.from_messages([("system", system_prompts), ("placeholder", "{messages}")])
         self._agent = create_react_agent(_llm, sql_toolkit.get_tools(), prompt=prompt_template, checkpointer=MemorySaver())
         self._config = {"configurable": {"thread_id": os.path.basename(cache_urn_dir)}}
@@ -27,9 +32,12 @@ class SqliteAgent:
         self._log(f"User: {prompt}")
         responses = []
         async for step in self._agent.astream({"messages": [("human", prompt)]}, self._config, stream_mode="updates"):
-            self._log(f"Assistant: {step}")
             if "agent" in step:
                 for message in step["agent"]["messages"]:
+                    self._log(message.pretty_repr())
                     if isinstance(message.content, str) and message.content:
                         responses.append(message.content)
+            if "tools" in step:
+                for message in step["tools"]["messages"]:
+                    self._log(message.pretty_repr())
         return responses
